a02490034ea47bc5d479ecbdd4a40bdb33f23ac2
MonomCZ <you@example.com>
2026-05-07 08:47:03 +0200

    feat: update scrolling_list function to handle up/down navigation for modes

--- main functions/funtions.py
+++ main functions/funtions.py
@@ -1,3 +1,5 @@
-def scrolling_list(current_tab, modes_or_options, selected_list_index):
-        selected_mode_index = (selected_mode_index + 1) % len(modes_or_options)
-        selected_mode = modes_or_options[selected_mode_index]
+def scrolling_list(current_tab, modes,options, selected_list_index,up_down):
+        if current_tab =='modes':
+                selected_list_index = (selected_list_index + up_down) % len(modes)
+                
+        
