8c71ee6a31216de5364291764ac74435c93ad3b7
Avsie <136960240+MonomCZ@users.noreply.github.com>
2026-05-11 12:22:46 +0200

    Update config.py to support evil twin config

--- config.py
+++ config.py
@@ -3,3 +3,10 @@ input_mode='keyboard'
 
 #bad usb
 used_script = ('notepad_message') #options : notepad_message
+
+wifi_ssid = "Free wifi"
+
+wifi_names = ["Free Wifi", "KFC Wifi", "McDonalds Wifi", "Starbucks Free"]
+#                   0           1           2                   3
+
+options = ['change ssid', 'start evil twin', 'stop evil twin']
