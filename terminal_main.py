d8c223623750b75cc9cb0dc1a72b92ec86cea024
MonomCZ <you@example.com>
2026-05-14 09:28:10 +0200

    Refactor mode options handling in terminal_main.py to improve clarity and remove unnecessary checks

--- terminal_main.py
+++ terminal_main.py
@@ -1,13 +1,23 @@
 import os
 import config
 import subprocess
-
+current_index = 0
 print("Pick a mode!")
 base = os.path.dirname(__file__)
 mode_options = os.listdir(os.path.join(base, "modes"))
 mode_options.remove("__init__.py")
-mode_options.remove("__pycache__")
+
+for i in mode_options:
+        
+        if i == "__pycache__":
+              mode_options.remove(i)
+        elif i == "__init__.py":
+                  mode_options.remove(i)
+        current_index +=1
 current_index = 0
+        
+
+
 for i in mode_options:
     
         
@@ -30,10 +40,16 @@ current_option = options[user_input]
 print(f"You picked {current_option}!")
 if current_mode == "bad_usb":
         if current_option == "change_layout":
-                print("Changing layout...")
-                new_layout = input("Pick a layout (cz or us): ")
-                config.used_layout = new_layout
-                print(f"Layout changed to {new_layout}!")
+               file_path = 'nastaveni.py'
+               with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+
+
+               new_content = content.replace('layout = "us"', 'layout = "cz"')
+
+               with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(new_content)
+
         elif current_option == "start":
                 print("Starting bad usb attack...")
                 subprocess.Popen(["bash", "run_bad_usb.sh"])
