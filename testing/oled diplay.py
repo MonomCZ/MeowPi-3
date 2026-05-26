61a36d66cf7f09101d97b23203cde0cc14c1710e
MonomCZ <vitek.monom@gmail.com>
2026-05-26 21:47:19 +0200

    Refactor OLED display script to improve structure and maintainability

--- testing/oled diplay.py
+++ testing/oled diplay.py
@@ -1,3 +1,4 @@
+
 from PIL import Image, ImageDraw
 import board
 import busio
@@ -17,35 +18,11 @@ draw = ImageDraw.Draw(image)
 
 number=1
 while True:
-
+    draw.rectangle((0, 0, WIDTH, HEIGHT), outline=0, fill=0)  # clear image
     draw.text((0, 0), str(number), fill=255)
     number+=1
     time.sleep(1)
 
-    oled.fill(0)from PIL import Image, ImageDraw
-import board
-import busio
-import adafruit_ssd1306
-import time
-
-WIDTH = 128
-HEIGHT = 64
-
-i2c = busio.I2C(board.SCL, board.SDA)
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
-oled.fill(0)
-oled.show()
-
-image = Image.new("1", (WIDTH, HEIGHT))
-draw = ImageDraw.Draw(image)
-
-number=1
-while True:
-    draw.rectangle((0, 0, WIDTH, HEIGHT), outline=0, fill=0)
-    draw.text((0, 0), str(number), fill=255)
-    number+=1
-    time.sleep(1)
-
-    
+    oled.fill(0)
     oled.image(image)
     oled.show()
