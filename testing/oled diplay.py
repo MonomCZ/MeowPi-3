558428ec3c2f1c8c9c70b6491d309fd0f0a0729f
MonomCZ <vitek.monom@gmail.com>
2026-05-26 22:23:12 +0200

    Add font specification to ASCII art rendering on OLED display

--- testing/oled diplay.py
+++ testing/oled diplay.py
@@ -1,4 +1,4 @@
-
+from PIL import ImageFont
 from PIL import Image, ImageDraw
 import board
 import busio
@@ -7,6 +7,8 @@ import time
 
 WIDTH = 128
 HEIGHT = 64
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 8)
+
 
 i2c = busio.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
@@ -26,7 +28,7 @@ while True:
 
     y = 0
     for line in ascii_art.split("\n"):
-        draw.text((0, y), line, fill=255)
+        draw.text((0, y), line, fill=255, font=font)
         y += 8
 
     oled.fill(0)
