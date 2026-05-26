47761815a38507489e1965ebc8d2d1f739875915
MonomCZ <vitek.monom@gmail.com>
2026-05-26 22:35:14 +0200

    Refactor ASCII art handling for OLED display with blinking effects

--- testing/oled diplay.py
+++ testing/oled diplay.py
@@ -4,6 +4,7 @@ import board
 import busio
 import adafruit_ssd1306
 import time
+import random
 
 WIDTH = 128
 HEIGHT = 64
@@ -20,8 +21,17 @@ draw = ImageDraw.Draw(image)
 
 number=1
 
-ascii_art = """\
+default_eyes = """\
   (=ↀωↀ=) """
+blink_left_eye = """
+  (=~_ↀ=) """
+blink_right_eye = """
+  (=ↀ_~=) """
+blink_both_eyes = """
+  (=~_~=) """
+faces=[blink_left_eye,blink_right_eye,blink_both_eyes]
+
+ascii_art = default_eyes
 
 while True:
     draw.rectangle((0, 0, WIDTH, HEIGHT), outline=0, fill=0)  # clear image
@@ -30,6 +40,19 @@ while True:
     for line in ascii_art.split("\n"):
         draw.text((0, y), line, fill=255, font=font)
         y += 8
+
+    if blinking:
+        time.sleep(blink_timer)
+        ascii_art = default_eyes
+        blinking = False
+        no_blinking_timer = random.randint(0.5, 3)
+
+    if not blinking:
+        time.sleep(no_blinking_timer)
+        ascii_art = random.choice(faces)
+        blinking=True
+        blink_timer = random.randint(0.1, 1)
+
 
     oled.fill(0)
     oled.image(image)
