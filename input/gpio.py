93c78ac4a748f918e3b9818e38de073dfb38d53b
MonomCZ <vitek.monom@gmail.com>
2026-05-27 08:53:42 +0200

    Refactor GPIO setup and button input handling in gpio.py

--- input/gpio.py
+++ input/gpio.py
@@ -3,9 +3,11 @@ import time
 
 btn = 22
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+while True:
 
-if GPIO.input(btn) == 0:
-    print("Button is pressed")
-    time.sleep(0.2)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    if GPIO.input(btn) == 1:
+        print("Button is pressed")
+        time.sleep(0.2)
