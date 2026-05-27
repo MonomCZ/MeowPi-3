67baf972c064456b064a0965dbc42f95157b1043
MonomCZ <vitek.monom@gmail.com>
2026-05-27 09:12:36 +0200

    Add delay in main loop for smoother OLED display updates

--- testing/button_and_display.py
+++ testing/button_and_display.py
@@ -25,6 +25,7 @@ draw = ImageDraw.Draw(image)
 while True:
     draw.rectangle((0, 0, WIDTH, HEIGHT), fill=0)
     draw.text((0, 0), str(number), fill=255, font=font)
+    time.sleep(0.2)
     if gpio_input.button1():
         print("Button 1 pressed")
         number+=1
