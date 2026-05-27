7464d4569af8a4b6b9671641ef1666c40c579bfc
MonomCZ <vitek.monom@gmail.com>
2026-05-27 09:05:25 +0200

    Add GPIO input handling and OLED display functionality

---
+++ input/gpio_input.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+btn1 = 22
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(btn1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def button1():
+    while True:
+        if GPIO.input(btn1) == 1:
+            return True
+            break
+        
---
+++ testing/button_and_display.py
@@ -0,0 +1,35 @@
+from PIL import ImageFont
+from PIL import Image, ImageDraw
+import board
+import busio
+import adafruit_ssd1306
+import time
+import random
+import input.gpio_input as gpio_input
+
+WIDTH = 128
+HEIGHT = 64
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 20)
+
+number=1
+
+i2c = busio.I2C(board.SCL, board.SDA)
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
+oled.fill(0)
+oled.show()
+number=1
+image = Image.new("1", (WIDTH, HEIGHT))
+draw = ImageDraw.Draw(image)
+
+
+while True:
+    draw.rectangle((0, 0, WIDTH, HEIGHT), fill=0)
+    draw.text((0, 0), number, fill=255, font=font)
+    if gpio_input.button1():
+        number+=1
+
+
+
+
+oled.image(image)
+oled.show()
