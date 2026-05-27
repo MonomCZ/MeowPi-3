9cd6f1829685632f56392612266fc1e0ec1c8041
MonomCZ <vitek.monom@gmail.com>
2026-05-27 09:53:17 +0200

    pushed text to the bottom

--- testing/button_and_display.py
+++ testing/button_and_display.py
@@ -28,13 +28,13 @@ oled.show()
 while True:
     draw.rectangle((0, 0, WIDTH, HEIGHT), fill=0)
     if number > 11:
-        draw.text((0, 0), 'Bangaranga!', fill=255, font=font)
+        draw.text((0, 10), 'Bangaranga!', fill=255, font=font)
         time.sleep(0.00001)
         number=0
 
     else:
         if not number==11:
-            draw.text((0, 0), str(number), fill=255, font=font)
+            draw.text((0, 10), str(number), fill=255, font=font)
         time.sleep(0.1)
         if gpio_input.button1():
             number+=1
