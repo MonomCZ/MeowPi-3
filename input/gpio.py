11f3fb73ff92127dc94da19d45c0335232db44a8
MonomCZ <vitek.monom@gmail.com>
2026-05-27 08:51:14 +0200

    Add GPIO button input handling to gpio.py

---
+++ input/gpio.py
@@ -0,0 +1,11 @@
+import RPI.GPIO as GPIO
+import time
+
+btn = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+if GPIO.input(btn) == 0:
+    print("Button is pressed")
+    time.sleep(0.2)
