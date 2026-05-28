8a5c8b33fa48fc229f286f0796496381300031dc
MonomCZ <vitek.monom@gmail.com>
2026-05-28 20:40:14 +0200

    fixed function name

--- temp/badusb_main.py
+++ temp/badusb_main.py
@@ -2,9 +2,10 @@ import ui.ui as ui
 import time
 
 
-ui.display_text('Welcome to bad usb meow pi thingy!!!', 0)
+ui.display_text('Welcome to bad usb', 0)
+ui.display_text('meow pi thingy!!!', 8)
 time.sleep(3)
 
-ui.clear_screen()
+ui.clear()
 ui.display_text("hold the button to start the attack", 20)
 
