f0e48950a986af1391bc395ccdaa67b1646167d5
MonomCZ <vitek.monom@gmail.com>
2026-05-28 21:36:15 +0200

    testing auto list of options for badusb

--- temp/badusb_main.py
+++ temp/badusb_main.py
@@ -1,6 +1,8 @@
 import ui.ui as ui
+import input.gpio_input as gpio_input
 import time
 import config as config
+
 
 ui.clear()
 
@@ -10,23 +12,33 @@ ui.display_text('Welcome to bad usb', 0)
 ui.display_text('meow pi thingy!!!', 20)
 time.sleep(3)
 
-ui.clear()
-ui.display_text("hold to start", 20)
+while True:
+    ui.clear()
+    ui.display_text("hold 1s to start", 20)
 
-ui.display_text("press to switch the mode", 40)
+    ui.display_text("press to switch the mode", 40)
 
-ui.display_temperature()
+    ui.display_temperature()
 
-if not len(config.used_script) < 20:
+    if not len(config.used_script) < 20:
+        displayed_used_script = config.used_script[:20]
+        mode_cut=True
+    else:
+        mode_cut=False
+        displayed_used_script = config.used_script[:20]
+
     displayed_used_script = config.used_script[:20]
-    mode_cut=True
-else:
-    mode_cut=False
-    displayed_used_script = config.used_script[:20]
-
-displayed_used_script = config.used_script[:20]
-if mode_cut:
-    displayed_used_script+="..."
+    if mode_cut:
+        displayed_used_script+="..."
 
 
-ui.display_text(displayed_used_script, 0)
+    ui.display_text(displayed_used_script, 0)
+    if gpio_input.button1():
+        sleep(0.2)
+        press=True
+        sleep(0.8)
+        if gpio_input.button1():
+        
+
+        
+
