59a5486a3f7db1bd422b8b8432c53c4841235395
MonomCZ <vitek.monom@gmail.com>
2026-05-28 20:38:39 +0200

    started working on a testing version of the ui

--- temp/badusb_main.py
+++ temp/badusb_main.py
@@ -1,3 +1,10 @@
 import ui.ui as ui
+import time
 
-ui.display_text('hello world', 0)
+
+ui.display_text('Welcome to bad usb meow pi thingy!!!', 0)
+time.sleep(3)
+
+ui.clear_screen()
+ui.display_text("hold the button to start the attack", 20)
+
