f1f4e1deb7df3b9ae52afaa97e15da05a657ae39
MonomCZ <vitek.monom@gmail.com>
2026-05-28 21:37:18 +0200

    small fix

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ input_mode='keyboard'
 
 #bad usb
 bad_usb_options = ['change_layout','change_script','start','stop']
-bad_usb_scripts = os.listdir('~/MeowPi-3/modes/bad_usb/bad_usb_scripts')
+bad_usb_scripts = os.listdir('~/MeowPi-3/modes/bad_usb/bad_usb_scripts/')
 print(bad_usb_scripts)
 used_script = ('notepad_message') #options : notepad_message , bee_movie_script
 used_layout = 'cz' #cz or us
