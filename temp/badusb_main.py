690e7b778c75aa17bd250cf5b355f278573f88e2
MonomCZ <vitek.monom@gmail.com>
2026-05-28 20:56:26 +0200

    more testing

--- temp/badusb_main.py
+++ temp/badusb_main.py
@@ -1,15 +1,15 @@
 import ui.ui as ui
 import time
 ui.clear()
-ui.display_text('1234567891234567891^23456789', 0)
 
 
-# ui.display_text('Welcome to bad usb', 0)
-# ui.display_text('meow pi thingy!!!', 20)
-# time.sleep(3)
 
-# ui.clear()
-# ui.display_text("hold the button to start", 0)
-# ui.display_text("the attack", 20)
-# ui.display_text("and press it to switch the currency mode", 40)
+ui.display_text('Welcome to bad usb', 0)
+ui.display_text('meow pi thingy!!!', 20)
+time.sleep(3)
+
+ui.clear()
+ui.display_text("hold the button to start ^the attack", 20)
+
+ui.display_text("and press it to switch the current mode", 40)
 
