bcd91b2edd04c72b35ef46882ae61148798061d0
MonomCZ <vitek.monom@gmail.com>
2026-05-28 20:44:08 +0200

    testing how many character fit on the display

--- temp/badusb_main.py
+++ temp/badusb_main.py
@@ -1,12 +1,15 @@
 import ui.ui as ui
 import time
-
+ui.clear()
+ui.display_text('123456789123456789', 0)
+break
 
 ui.display_text('Welcome to bad usb', 0)
 ui.display_text('meow pi thingy!!!', 20)
 time.sleep(3)
 
 ui.clear()
-ui.display_text("hold the button to start", 20)
-ui.display_text("the attack", 28)
+ui.display_text("hold the button to start", 0)
+ui.display_text("the attack", 20)
+ui.display_text("and press it to switch the currency mode", 40)
 
