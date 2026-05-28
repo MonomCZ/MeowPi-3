bdd5af32cbf8107a19892abee3552a2766a8a288
MonomCZ <vitek.monom@gmail.com>
2026-05-28 20:41:36 +0200

    fixed formatting

--- temp/badusb_main.py
+++ temp/badusb_main.py
@@ -3,9 +3,10 @@ import time
 
 
 ui.display_text('Welcome to bad usb', 0)
-ui.display_text('meow pi thingy!!!', 8)
+ui.display_text('meow pi thingy!!!', 20)
 time.sleep(3)
 
 ui.clear()
-ui.display_text("hold the button to start the attack", 20)
+ui.display_text("hold the button to start", 20)
+ui.display_text("the attack", 28)
 
