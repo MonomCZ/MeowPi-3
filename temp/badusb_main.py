d7002c0de8c25c0262341e47b84a4b234f9270d5
MonomCZ <vitek.monom@gmail.com>
2026-05-28 21:29:19 +0200

    made the text even shorter

--- temp/badusb_main.py
+++ temp/badusb_main.py
@@ -11,7 +11,7 @@ ui.display_text('meow pi thingy!!!', 20)
 time.sleep(3)
 
 ui.clear()
-ui.display_text("hold button to start", 20)
+ui.display_text("hold to start", 20)
 
 ui.display_text("press to switch the mode", 40)
 
