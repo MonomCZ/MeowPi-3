033c3f589c9ce726584035973d6296fa0f4b4526
MonomCZ <vitek.monom@gmail.com>
2026-05-28 21:21:49 +0200

    made the mode name auto cut

--- temp/badusb_main.py
+++ temp/badusb_main.py
@@ -16,5 +16,15 @@ ui.display_text("hold button to start", 20)
 ui.display_text("press to switch the mode", 40)
 
 ui.display_temperature()
+if not len(config.used_script) > 17:
+    displayed_used_script = config.used_script[:17]
+    mode_cut=True
+else:
+    mode_cut=False
+
+displayed_used_script = config.used_script[:17]
+if mode_cut:
+    displayed_used_script.append("...")
+
 
 ui.display_text(config.used_script, 0)
